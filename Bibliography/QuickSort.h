1f076577615cb6a1b83f2d341f0cdc8520e563a7
Salvatore Sramante <salvatore.bramante@yahoo.it>
2021-05-02 17:36:04 +0200

    Correct template linking error and bad type variable error in swap function

--- Bibliography/QuickSort.h
+++ Bibliography/QuickSort.h
@@ -12,16 +12,41 @@
 #include <vector>
 
 template <class T>
-void quickSort(std::vector<T>& A, int p, int r);
+void quickSort(std::vector<T>& A, int p, int r) {
+    if (p < r) {
+        int q = partition(A, p, r);
+        quickSort(A, p, q);
+        quickSort(A, q+1, r);
+    }
+}
 //template <class T>
 //void quickSort(std::vector<T>& A);
 
 template <class T>
-int partition(std::vector<T>& A, int p, int r);
-
+int partition(std::vector<T>& A, int p, int r) {
+    T pivot = A.at(p);
+    int i = p-1;
+    int j = r+1;
+    while (true) {
+        do {
+            j--;
+        } while (A.at(j) > pivot);
+        do {
+            i++;
+        } while (A.at(i) < pivot);
+        if (i < j) {
+            swap(A, i, j);
+        } else {
+            return j;
+        }
+    }
+}
 template <class T>
-void swap(std::vector<T>& A, int i, int j);
-
+void swap(std::vector<T>& A, int i, int j) {
+    T temp = A.at(i);
+    A.at(i) = A.at(j);
+    A.at(j) = temp;
+}
 
 
 
