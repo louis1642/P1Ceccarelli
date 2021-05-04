10dd3ed55201a6d7601401951165316ab39cd83f
Luigi Catello <luigicatello16@gmail.com>
2021-05-04 11:14:07 +0200

    Completato esercizio sulla Bibliography

--- Bibliography/main.cpp
+++ Bibliography/main.cpp
@@ -20,19 +20,11 @@ using std::vector;
 
 int main() {
 
-//    Publication* pub1 = new PaperArticle( "il titolo", "Franco Balordo", 1992, "Focus", 23, 130, 132);
-//    Publication* pub2 = new PaperArticle("s.v.lo", "Antonio Lello", 1981, "Nature", 3, 12, 20);
-//
-//    PaperArticle art1("il titolo", "Franco Balordo", 1992, "Focus", 23, 130, 132);
-//    PaperArticle art2("s.v.lo", "Antonio Lello", 1981, "Nature", 3, 12, 20);
-//
-//    cout << (*pub1 < *pub2) << endl;
-
-    // TODO vettore di publication*, calcolare totale per ogni categoria (con contatore e con casting), ordinare il vettore e stampare
     vector<Publication*> pubs{
         new PaperArticle( "Algorithms and Data Structures", "Zeccarelli M.", 1992, "Focus", 23, 130, 132),
         new ConferenceArticle("Articolo Conf", "Celentano G., Celentano L.", 2010, "IEEE conference", "Roma", 17),
-        new Book("Fondamenti di controlli", "Bolzern P., Scattolini R.", 2009, "McGraw Hill", "978-98-232-4356-1")
+        new Book("Fondamenti di controlli", "Bolzern P., Scattolini R.", 2009, "McGraw Hill", "978-98-232-4356-1"),
+        new Book("Respiro", "Chiang T.", 2018, "TizioCaio", "987-12-345-5678-9")
     };
 
     // stampa preliminare
@@ -40,18 +32,48 @@ int main() {
     for (const auto pubPtr : pubs) {
         cout << pubPtr->getTitle() << endl;
     }
-    std::clog << "log\n";
+
+    // ordinamento vettore
     quickSort(pubs, 0, pubs.size()-1);
 
+    // stampa vettore ordinato
     cout << "\nVettore ordinato:" << endl;
     for (const auto pubPtr : pubs) {
         cout << pubPtr->getTitle() << endl;
     }
 
-    cout << endl << (*pubs.at(0) < *pubs.at(1)) << endl;
-    cout << endl << (*pubs.at(1) < *pubs.at(2)) << endl;
-    cout << endl << (*pubs.at(2) < *pubs.at(0)) << endl;
+    // contatore numero pubblicazioni
+    int totalePaper{0}, totaleConference{0}, totaleBook{0};
 
+    // contiamo le pubblicazioni per tipo utilizzando il downcasting
+    // (poteva essere fatto nel ciclo for della stampa)
+    for (const auto pubPtr : pubs) {
+        auto paperPtr = dynamic_cast<PaperArticle*>(pubPtr);
+        if (paperPtr != nullptr) {
+            // è un PaperArticle
+            totalePaper++;
+            continue;   // se è un PaperArticle è inutile continuare a testare
+        }
+        auto conferencePtr = dynamic_cast<ConferenceArticle*>(pubPtr);
+        if (conferencePtr != nullptr) {
+            // è un ConferenceArticle
+            totaleConference++;
+            continue;
+        }
+        // esistendo solo tre tipi, se non era uno dei precedenti sicuramente sarà Book...
+        // lascio la struttura nel caso un giorno venissero aggiunti altri tipi
+        auto bookPtr = dynamic_cast<Book*>(pubPtr);
+        if (bookPtr != nullptr) {
+            // è un Book
+            totaleBook++;
+        }
+    }       // end for
+    // al posto di usare i dynamic_cast, si poteva utilizzare la funzione getType() ed effettuare uno switch
+
+    cout << endl;
+    cout << "Totale Paper Article:      " << totalePaper << endl;
+    cout << "Totale Conference Article: " << totaleConference << endl;
+    cout << "Totale Book:               " << totaleBook << endl;
 
 
 
